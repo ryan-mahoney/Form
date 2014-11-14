49a000b5af68139da002f6e1d6d922fe892d712f
Ryan Mahoney <ryan@virtuecenter.com>
2014-11-14 08:48:01 -0500

    fix bundle paths

--- src/Controller.php
+++ src/Controller.php
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -100,8 +100,8 @@ class Controller {
     public function bundleHtml ($bundle, $form, $dbURI=false) {
         $this->view->html(
             $this->service->markerToClass($bundle . '__Form__' . $form),
-            'bundles/' . $bundle . '/app/forms/' . strtolower($form) . '.yml',
-            'public/layouts/' . $bundle . '/forms/' . strtolower($form) . '.html',
+            [strtolower($bundle) . '/forms/' . strtolower($form) . '.yml', $bundle . '/forms/' . strtolower($form) . '.yml'],
+            [strtolower($bundle) . '/forms/' . strtolower($form) . '.html', $bundle . '/forms/' . strtolower($form) . '.html'],
             $dbURI
         );
     }
